f61211b5bc943064a24cdee1ac0244fffc737325
Sol Jennings <sjennings@bunnings.com.au>
2024-03-23 01:06:47 +1100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
@@ -17,11 +17,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (<>
-  <SpeedInsights/>
-    <Analytics/>
+  return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html></>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
   );
 }
